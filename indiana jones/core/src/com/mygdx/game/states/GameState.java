4368390516042eb499eb76b4a11284445394207f
kazmanfima <briantran@rocketmail.com>
2016-05-15 16:28:13 -0400

    Add files via upload

---
+++ indiana jones/core/src/com/mygdx/game/states/GameState.java
@@ -0,0 +1,59 @@
+package com.mygdx.game.states;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.MainGame;
+
+import managers.GameStateManager;
+
+/**
+ * An abstract class that once implemented, represents a GameState that is sorted
+ * using the GameStateManager class.
+ * 
+ * @author Brian Tran
+ * @version 1.0 13/05/2016
+ *
+ * <p>
+ * <b>gsm</b> Instance of GameStateManager used to manage game states
+ * <p>
+ * <b>game</b> Instance of MainGame used to manage current screen of game
+ * <p>
+ * <b>batch</b> Instance of SpriteBatch used as a container for all screen elements
+ */
+public abstract class GameState {
+	
+	GameStateManager gsm;
+	MainGame game;
+	
+	protected SpriteBatch batch;
+	
+	/**
+	 * The GameState constructor sets the GameStateManager, game, and batch to
+	 * ones that are passed in.
+	 * 
+	 * @param gsm Manages game states
+	 */
+	protected GameState (GameStateManager gsm){
+		this.gsm = gsm;
+		game = gsm.getGame();
+		batch = game.getSpriteBatch();
+	}
+	
+	/**
+	 * Unused abstract method.
+	 */
+	public abstract void handleInput();
+	/**
+	 * Unused abstract method.
+	 * @param dt Delta time of frames
+	 */
+	public abstract void update(float dt);
+	/**
+	 * Unused abstract method.
+	 */
+	public abstract void render();
+	/**
+	 * Unused abstract method.
+	 */
+	public abstract void dispose();
+
+}
--- indiana jones/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ indiana jones/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -3,7 +3,12 @@ package com.mygdx.game.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.MainGame;
-
+/**
+ * The DesktopLauncher class contains all of the information needed to run
+ * this application on a desktop computer. Using the config class, information
+ * is stored, such as resolution and other video options. The MainGame class is
+ * called, which starts the program.
+ */
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
@@ -12,3 +17,4 @@ public class DesktopLauncher {
 		new LwjglApplication(new MainGame(), config);
 	}
 }
+ 
