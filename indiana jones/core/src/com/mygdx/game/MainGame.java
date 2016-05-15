f7bf6a3a8fde8e21084e336434de9e69c837ac82
kazmanfima <briantran@rocketmail.com>
2016-05-15 18:09:24 -0400

    Add files via upload
    now with:
    - memory optimization

--- indiana jones/core/src/com/mygdx/game/MainGame.java
+++ indiana jones/core/src/com/mygdx/game/MainGame.java
@@ -1,34 +1,88 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Pixmap;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+/**
+ * The MainGame, which extends the Game class, is currently responsible for
+ * starting the execution of the application by calling upon the MainMenu class.
+ * 
+ * @author Brian Tran
+ * @version 1.0 13/05/2016
+ *
+ *          <p>
+ *          <b>Instance Variables</b>
+ *          <p>
+ *          <b>batch</b> Acts as a container for onscreen elements
+ *
+ */
 public class MainGame extends Game {
- SpriteBatch batch;
- Texture img;
- 
- @Override
- public void create () {
-	 //sets the cursor to a custom image
-  Pixmap pm = new Pixmap(Gdx.files.internal("cursor.png"));
-  Gdx.graphics.setCursor(Gdx.graphics.newCursor(pm, 0, 0));
-  pm.dispose();
-  
-  batch = new SpriteBatch();
-  mainMenu();
- }
- 
- public void mainMenu(){
-  setScreen (new MainMenu (this));
- }
 
- @Override
- public void render () {
-  super.render();
- }
+	Pixmap pm;
+
+	/**
+	 * create() is an overridden method that is responsible for setting the
+	 * cursor of the game to a custom texture, as well as initializing the batch
+	 * variable and calling upon the mainMenu() method.
+	 */
+	@Override
+	public void create() {
+		// sets the cursor to a custom image
+		pm = new Pixmap(Gdx.files.internal("cursor.png"));
+		Gdx.graphics.setCursor(Gdx.graphics.newCursor(pm, 0, 0));
+		pm.dispose();
+
+		splashScreen();
+	}
+
+	/**
+	 * Sets the current screen to the MainMenu screen.
+	 */
+	public void splashScreen() {
+		setScreen(new SplashScreen(this));
+	}
+
+	/**
+	 * Calls the constructor of the Game class.
+	 */
+	@Override
+	public void render() {
+		super.render();
+	}
+
+	/**
+	 * Unused overridden method.
+	 */
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Unused overridden method.
+	 */
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Unused overridden method.
+	 */
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Unused overridden method.
+	 */
+	@Override
+	public void dispose() {
+	}
+
 }
---
+++ indiana jones/core/src/com/mygdx/game/SplashScreen.java
@@ -0,0 +1,152 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+import aurelienribon.tweenengine.BaseTween;
+import aurelienribon.tweenengine.Tween;
+import aurelienribon.tweenengine.TweenCallback;
+import aurelienribon.tweenengine.TweenManager;
+import tween.SpriteManager;
+
+public class SplashScreen implements Screen {
+
+	SpriteBatch batch;
+
+	MainGame game;
+
+	Animation walkAnim;
+	Animation rollAnim;
+
+	TextureRegion[] framesWalk;
+	TextureRegion[] framesRoll;
+
+	Sprite splashTitleSprite;
+
+	Texture walk;
+	Texture roll;
+	Texture background;
+	Texture splashTitle;
+
+	float time;
+	float speed;
+	float walkX;
+
+	TweenManager tweenManager;
+
+	public SplashScreen(MainGame game) {
+		this.game = game;
+	}
+
+	@Override
+	public void show() {
+		batch = new SpriteBatch();
+		tweenManager = new TweenManager();
+		Tween.registerAccessor(Sprite.class, new SpriteManager());
+
+		speed = 250.0f;
+
+		background = new Texture("splash.png");
+
+		splashTitle = new Texture("splashtitle.png");
+		splashTitleSprite = new Sprite(splashTitle);
+		splashTitleSprite.setPosition(0f, 768 - splashTitleSprite.getHeight());
+
+		roll = new Texture("roll_left.png");
+		walk = new Texture("left_walk.png");
+
+		TextureRegion[][] temp_left = TextureRegion.split(walk, 32, 64);
+		TextureRegion[][] temp_roll = TextureRegion.split(roll, 32, 32);
+		framesWalk = new TextureRegion[4];
+		framesRoll = new TextureRegion[4];
+
+		int index = 0;
+
+		for (int rows = 0; rows < 2; rows++) {
+			for (int col = 0; col < 2; col++) {
+				framesWalk[index] = temp_left[rows][col];
+				framesRoll[index] = temp_roll[rows][col];
+				index++;
+			}
+		}
+
+		walkAnim = new Animation(0.25f, framesWalk);
+		rollAnim = new Animation(0.25f, framesRoll);
+
+		Tween.set(splashTitleSprite, SpriteManager.ALPHA).target(0f).start(tweenManager);
+		Tween.to(splashTitleSprite, SpriteManager.ALPHA, 2f).target(1f).repeatYoyo(1, 4f)
+		.setCallback(new TweenCallback() {
+			@Override
+			public void onEvent(int arg0, BaseTween<?> arg1) {
+				dispose();
+				((Game) Gdx.app.getApplicationListener()).setScreen(new MainMenu(batch, game));
+			}
+		}).start(tweenManager);
+
+		Gdx.gl.glClearColor(.8f, .8f, .8f, 1);
+	}
+
+	@Override
+	public void render(float delta) {
+
+		tweenManager.update(delta);
+
+		time += Gdx.graphics.getDeltaTime();
+		batch.begin();
+		// TODO Auto-generated method stub
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		batch.draw(background, 0, 0, 1200f, 768f);
+
+		walkX += Gdx.graphics.getDeltaTime() * speed;
+
+		batch.draw(walkAnim.getKeyFrame(time, true), 1300 - walkX, 100, 64f, 128f);
+		batch.draw(rollAnim.getKeyFrame(time, true), 1450 - walkX, 100, 128f, 128f);
+
+		splashTitleSprite.draw(batch);
+
+		batch.end();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		walk.dispose();
+		roll.dispose();
+		background.dispose();
+		splashTitle.dispose();
+
+	}
+
+}
