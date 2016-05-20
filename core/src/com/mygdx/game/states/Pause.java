9ddd5c487e7b80b9be11c79e4ab1480b381c42c4
frankisdank <xtraproblem@hotmail.com>
2016-05-20 10:54:08 -0400

    maps

---
+++ core/src/com/mygdx/game/states/Pause.java
@@ -0,0 +1,35 @@
+package com.mygdx.game.states;
+
+import managers.GameStateManager;
+
+public class Pause extends GameState {
+	
+	public Pause(GameStateManager gsm){
+		super (gsm);
+	}
+
+	@Override
+	public void handleInput() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(float dt) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
