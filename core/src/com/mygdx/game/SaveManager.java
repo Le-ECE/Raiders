47bd44c158ca0b35545f000d46e6f5eff894b3ee
frankisdank <xtraproblem@hotmail.com>
2016-06-09 02:35:58 -0400

    Revert "D branch 42"

--- core/src/com/mygdx/game/SaveManager.java
+++ core/src/com/mygdx/game/SaveManager.java
@@ -4,8 +4,10 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Calendar;
 
 import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileSystemView;
 
 /**
@@ -73,15 +75,26 @@ public class SaveManager {
 		}
 		choose.setCurrentDirectory(new File(newView.getDefaultDirectory() + "//High Noon Data//RaidersSave//saves"));
 
+		String saveName = "";
+
+		Calendar newCalendar = Calendar.getInstance();
+		newCalendar.setTimeInMillis(System.currentTimeMillis());
+
+		saveName = newCalendar.get(Calendar.DAY_OF_MONTH) + "~" + newCalendar.get(Calendar.MONTH) + "~"
+				+ newCalendar.get(Calendar.YEAR) + " " + newCalendar.get(Calendar.HOUR) + "«"
+				+ newCalendar.get(Calendar.MINUTE) + "«" + newCalendar.get(Calendar.SECOND);
+		String writeName = loadedSave.getName() + " " + saveName + MainGame.EXT;
 		try {
 			PrintWriter saveWriter = new PrintWriter(
-					new FileWriter("//" + choose.getCurrentDirectory() + "//" + "GameSave" + MainGame.EXT));
+					new FileWriter("//" + choose.getCurrentDirectory() + "//" + writeName));
 
 			saveWriter.println(loadedSave.getName());
 			saveWriter.println(loadedSave.getDifficulty());
 			saveWriter.println(loadedSave.getTimeSeconds());
 			saveWriter.close();
 		} catch (IOException e) {
+			// fix the goddamn cursor lock when switching
+			JOptionPane.showMessageDialog(null, "Error saving your gamestate.", "Attention", JOptionPane.ERROR_MESSAGE);
 		}
 	}
 }
